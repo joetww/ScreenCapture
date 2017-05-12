f6e64139fffda1cd2eff4d5ee511a61b301174da
Joetww <joetww@gmail.com>
2017-05-12 12:24:49 +0800

    精簡註解/去除設定window大小的部份

--- s.php
+++ s.php
@@ -3,23 +3,17 @@
 
     use Facebook\WebDriver\Remote\DesiredCapabilities;
     use Facebook\WebDriver\Remote\RemoteWebDriver;
-    //use Facebook\WebDriver\Remote\RemoteWebElement;
-    //use Facebook\WebDriver\WebDriverWindow;
 
     require_once('vendor/autoload.php');
 
-    // start Firefox with 5 second timeout
     $host = 'http://localhost:4444/wd/hub'; // this is the default
-    //$capabilities = DesiredCapabilities::chrome();
-    //$capabilities = DesiredCapabilities::firefox();
     $capabilities = DesiredCapabilities::phantomjs();
 
     $driver = RemoteWebDriver::create($host, $capabilities, 5000);
     # set the timeout for implicit waits as 10 seconds
     $driver->manage()->timeouts()->implicitlyWait = 10;
-    $size = new WebDriverDimension(300, 300);
-    $driver->manage()->window()->setSize($size);
-    //var_dump($driver->manage()->window()->getSize());
+//    $size = new WebDriverDimension(300, 300);
+//    $driver->manage()->window()->setSize($size);
 
     $driver->get('http://24h.pchome.com.tw/prod/DMBE05-A90081FBM');
     //$driver->get('http://24h.pchome.com.tw/prod/DMAG00-A90059M3Q?q=/S/DMAG0K');
@@ -41,16 +35,7 @@
         }, 'wait....timeout'
     );
     $driver->executeScript("document.body.bgColor = 'white';");
-//    $companyFoot = $driver->findElement(WebDriverBy::cssSelector('dl.msg_box > dd:nth-child(4)'));
-//    $companyHead = $driver->findElement(WebDriverBy::cssSelector('#HEADER'));
-//    $companyBody = $driver->findElement(WebDriverBy::cssSelector('body'));
-    //$size = new WebDriverDimension($companyBody->getLocation()->getX() + $companyBody->getSize()->getWidth() + 40, $companyBody->getLocation()->getY()+$companyBody->getSize()->getHeight() + 20);
-//    $size = new WebDriverDimension(1400, $companyBody->getLocation()->getY() + $companyBody->getSize()->getHeight() + 30);
-//    $size = new WebDriverDimension($companyHead->getLocation()->getX() + $companyHead->getSize()->getWidth() + 20, $companyFoot->getLocation()->getY()+$companyFoot->getSize()->getHeight() + 100);
-    //$size = new WebDriverDimension($companyHead->getLocation()->getX() + $companyHead->getSize()->getWidth() + 300, 32168 * 2);
-    //    $driver->manage()->window()->setSize($size);
     $driver->manage()->window()->maximize();
-    //var_dump($driver->manage()->window()->getSize());
     $full_screenshot = TakeScreenshot($driver, $capabilities);
 
     $driver->quit();
